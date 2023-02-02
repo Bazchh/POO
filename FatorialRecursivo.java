d0938416b2070f33704512bf05639f6901d4bed6
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-02-02 14:30:47 -0300

    Fatorial recursivo em java

---
+++ FatorialRecursivo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fatrecurs {
+ public static void main(String[] args) {
+    int n;
+    Scanner ent = new Scanner(System.in);
+    System.out.print("valor: ");
+    n = ent.nextInt();
+    System.out.println("O fatorial de "+n+" é: "+fat(n));
+    ent.close();
+ }   
+
+ public static int fat(int num){
+    if(num == 0){
+        return 1;
+    } else if(num > 0) {
+        return (num*fat(num-1));
+    }
+    return 0;
+ }
+
+}
+
+
