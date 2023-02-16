6a5686f6de3ff0a840e6fbf85e1fbf04780f9608
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-02-16 14:23:55 -0300

    como usar operador ternario

---
+++ DemoConta.java
@@ -0,0 +1,8 @@
+public class DemoConta{
+    public static void main(String[] args) {
+        ContaBancaria mimika = new ContaBancaria("Mimika", 1200, true);
+        ContaBancaria Joao = new ContaBancaria("Joao");
+
+        mimika.imprimeDados();
+    }
+}
