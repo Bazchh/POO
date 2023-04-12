50747fb2fbd44c5ecc1f66c47194cb5fccd7e5d8
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-12 13:52:25 -0300

    finalização

--- Colecoes/paciente/TestaConsultorio.java
+++ Colecoes/paciente/TestaConsultorio.java
@@ -1,3 +1,4 @@
+package paciente;
 
 import java.util.Random;
 import java.util.Timer;
@@ -7,27 +8,37 @@ public class TestaConsultorio {
     public static void main(String[] args) {
         Consultorio consultorio = new Consultorio();
         int delay = 5000;   // tempo de espera antes da 1ª execução da tarefa.
-        int interval = 24000;  // intervalo no qual a tarefa será executada.
+        int interval = 4000;  // intervalo no qual a tarefa será executada.
         Timer timer = new Timer();
-
+        Timer timer2 = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
         int RG;
         int idade;
         boolean atendimento = true;
+        boolean atendimento2 = true;
+        int atendidos;
         public void run() {
                 RG = geradorRandom();
                 idade = geradorRandom();
                 atendimento = consultorio.chegaPaciente(RG, idade);
-                timer.schedule(new TimerTask(){
+                System.out.println(consultorio.getQtdPacientes());
+                timer2.schedule(new TimerTask(){
                     public void run(){
-                        consultorio.atendePaciente();
+                        if(atendimento2 == true){
+                            atendimento2 = consultorio.atendePaciente();
+                            atendidos++;
+                            System.out.println(atendidos);
+                        }
+                        else if (atendimento2 == false){
+                            timer2.cancel();
+                        }
                     }
-                },interval+60000,interval+60000);
-                if(atendimento == false && consultorio.atendePaciente() == true){
+                },delay+2000,interval+6000);
+                if(atendimento == false && atendimento2 == false){
                     timer.cancel();
                 }    
             } 
-        }, interval, interval);    
+        }, delay, interval);    
     }
 
     public static int geradorRandom(){
