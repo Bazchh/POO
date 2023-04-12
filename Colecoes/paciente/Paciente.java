6403d3ae1ef5a795d378110746d7ba25841ca66f
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-12 13:19:57 -0300

    Update Paciente.java

--- Colecoes/paciente/Paciente.java
+++ Colecoes/paciente/Paciente.java
@@ -1,12 +1,17 @@
 package paciente;
 
-public class Paciente {
+public class Paciente  implements Comparable{
     protected int RG;
     protected int idade;
 
     Paciente(int RG, int idade) {
         this.RG = RG;
         this.idade = idade;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return idade;
     }
 
 }
