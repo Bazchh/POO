dcefd50bd437433f5e96aca93491284d93d2feb5
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-13 10:28:28 -0300

    Add files via upload

---
+++ ClassesAbstratas/FiguraGeometrica/Circulo.java
@@ -0,0 +1,9 @@
+package ClassesAbstratas.Geometria;
+
+public class Circulo extends FiguraGeometrica {
+
+    public void desenha() {
+        System.out.println("Desenhando um circulo");
+    }
+
+}
