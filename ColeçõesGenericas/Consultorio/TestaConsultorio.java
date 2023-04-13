f970b1d7205e3e5751d42b5d4f1b0f22828825e8
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2023-04-13 10:35:11 -0300

    Reorganização de arquivos

---
+++ ColeçõesGenericas/Consultorio/TestaConsultorio.java
@@ -0,0 +1,50 @@
+package paciente;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TestaConsultorio {
+    public static void main(String[] args) {
+        Consultorio consultorio = new Consultorio();
+        int delay = 5000;   // tempo de espera antes da 1ª execução da tarefa.
+        int interval = 4000;  // intervalo no qual a tarefa será executada.
+        Timer timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+        int RG;
+        int idade;
+        boolean atendimento = true;
+        boolean atendimento2 = true;
+        public void run() {
+                RG = geradorRandom();
+                idade = geradorRandom();
+                atendimento = consultorio.chegaPaciente(RG, idade);
+                System.out.println(consultorio.getQtdPacientes());
+                timer.schedule(new TimerTask(){
+                    public void run(){
+                        if(atendimento2 == true){
+                            atendimento2 = consultorio.atendePaciente();
+                            System.out.println(atendimento2);
+                        }
+                          
+                    }
+                },delay+2000,interval+6000);
+                if(atendimento == false && atendimento2 == false){
+                    System.out.print("Consultorio fechado!\n");
+                    timer.cancel();
+                }    
+            } 
+        }, delay, interval);    
+
+        
+    }
+
+    public static int geradorRandom(){
+        int n;
+        Random gerador = new Random();
+         n = gerador.nextInt(80);
+        return n;
+    }
+
+}
